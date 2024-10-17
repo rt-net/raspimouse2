d5a01f1ead928df36e6dfce68f915520986ff8a2
KuraZuzu <kurasawa@rt-net.jp>
2024-10-17 18:31:52 +0900

    Applied "isort"

--- raspimouse/launch/raspimouse.launch.py
+++ raspimouse/launch/raspimouse.launch.py
@@ -15,18 +15,14 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-
-from launch import LaunchDescription
-from launch.actions import EmitEvent
-from launch.actions import RegisterEventHandler
-from launch.events import Shutdown
-from launch.events import matches_action
-
 from launch_ros.actions import LifecycleNode
 from launch_ros.event_handlers import OnStateTransition
 from launch_ros.events import lifecycle
-
 from lifecycle_msgs.msg import Transition
+
+from launch import LaunchDescription
+from launch.actions import EmitEvent, RegisterEventHandler
+from launch.events import Shutdown, matches_action
 
 
 def generate_launch_description():
@@ -36,7 +32,7 @@ def generate_launch_description():
         package='raspimouse', executable='raspimouse', output='screen',
         parameters=[os.path.join(get_package_share_directory(
             'raspimouse'), 'config', 'params.yaml')]
-            )
+    )
 
     emit_configuring_event = EmitEvent(
         event=lifecycle.ChangeState(
